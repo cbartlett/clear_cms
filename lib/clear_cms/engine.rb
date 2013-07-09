4b0f51198a9262b5bbfadf53c073431b8e4d7610
Joel Niedfeldt <joel@niedfeldt.com>
2013-07-09 18:13:56 -0400

    requiring dependencies

--- lib/clear_cms/engine.rb
+++ lib/clear_cms/engine.rb
@@ -1,3 +1,10 @@
+require 'devise'
+require 'devise-encryptable'
+require 'carrierwave'
+require 'carrierwave/mongoid'
+require 'sunspot'
+require 'kaminari'
+
 module ClearCMS
   class Engine < ::Rails::Engine
     isolate_namespace ClearCMS
