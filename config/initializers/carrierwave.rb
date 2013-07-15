e579946b52597505ebe04e97134a593536ad4f8a
Joel Niedfeldt <joel@niedfeldt.com>
2013-07-15 12:07:22 -0400

    changing carrierwave back to load regular initializer, could also do this after initialize in the engine which might be nicer than the before initialize in the app

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,12 @@
 ::CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',       # required
-    :aws_access_key_id      => ENV['AWS_ACCESS_KEY'], #ClearCMS.config.aws_access_key,       # required
-    :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'] #ClearCMS.config.aws_secret_access_key,       # required
+    :aws_access_key_id      => ClearCMS.config.aws_access_key, #ENV['AWS_ACCESS_KEY'], #ClearCMS.config.aws_access_key,       # required
+    :aws_secret_access_key  => ClearCMS.config.aws_secret_access_key #ENV['AWS_SECRET_ACCESS_KEY'] #ClearCMS.config.aws_secret_access_key,       # required
     #:region                 => 'eu-west-1'  # optional, defaults to 'us-east-1'
   }
-  config.fog_directory  = ENV['FOG_DIRECTORY'] #Sidekiq is somehow loading the defaults, must be load order or thread issue?? #ClearCMS.config.fog_directory                     # required
-  config.asset_host       = ENV['ASSET_HOST'] #ClearCMS.config.asset_host            # optional, defaults to nil
+  config.fog_directory  = ClearCMS.config.fog_directory #ENV['FOG_DIRECTORY'] #Sidekiq is somehow loading the defaults, must be load order or thread issue?? #ClearCMS.config.fog_directory                     # required
+  config.asset_host       = ClearCMS.config.asset_host #ENV['ASSET_HOST'] #ClearCMS.config.asset_host            # optional, defaults to nil
   #config.fog_public     = true                                   # optional, defaults to true
   #config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 end
