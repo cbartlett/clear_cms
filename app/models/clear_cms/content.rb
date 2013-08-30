f15d2462ef9afc2543ccdaa50b0e866098f1a81d
Joel Niedfeldt <joel@niedfeldt.com>
2013-08-30 17:32:43 -0400

    removing indexes

--- app/models/clear_cms/content.rb
+++ app/models/clear_cms/content.rb
@@ -82,11 +82,11 @@ module ClearCMS
     index({updated_at: -1})
     index({created_at: -1})
     index({publish_at: -1, :_id=>-1})
-    index({publish_at: -1, :_type=>1})
-    index({categories: 1})
-    index({basename: 1})
-    index({tags: 1})
-    index({:_type=>1})
+    # index({publish_at: -1, :_type=>1})
+    # index({categories: 1})
+    # index({basename: 1})
+    # index({tags: 1})
+    # index({:_type=>1})
 
     #index({:_id=>-1}) #_id is indexed by default in mongodb
 
