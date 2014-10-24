c000a5c5ba294cc41e80ffd7fe3aad13afb44b0d
Ilana Seid <ilana@theline.com>
2014-10-24 16:59:02 -0400

    [#57007330] made raw block content type a requirement through validation

--- app/models/clear_cms/raw_block_validator.rb
+++ app/models/clear_cms/raw_block_validator.rb
@@ -1,10 +1,12 @@
-classRawBlockValidator < ActiveModel::EachValidator
-  def validate_each(record, attribute, value)
-  	content_block_types = []
-      record.attribute.each { |cb| content_block_types<<cb.type }
-      unless content_block_types.include?("raw")
-        record.errors[attribute] << (options[:message] || 'need at least one "raw" type')
-        return false
-      end
-  end
+module ClearCMS
+	class RawBlockValidator < ActiveModel::EachValidator
+		def validate_each(record, attribute, value)
+			content_block_types = []
+			record.content_blocks.each { |cb| content_block_types<<cb.type }
+			unless content_block_types.include?("raw")
+				record.errors[attribute] << (options[:message] || 'need at least one "raw" type')
+				return false
+			end
+		end
+	end
 end
