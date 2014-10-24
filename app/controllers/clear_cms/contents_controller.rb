363c23470ce3a30f53964fa84839f8831decc885
Ilana Seid <ilana@theline.com>
2014-10-24 17:02:12 -0400

    resetting the spacing in contents controller

--- app/controllers/clear_cms/contents_controller.rb
+++ app/controllers/clear_cms/contents_controller.rb
@@ -2,13 +2,13 @@ module ClearCMS
   class ContentsController < ClearCMS::ApplicationController
     require 'mail'
     #require 'mongoid'
-    before_filter :authenticate_user!, :except=>[:email]
-    
+    before_filter :authenticate_user!, :except=>[:email] 
     skip_before_filter :verify_authenticity_token, :only=>[:email]
     
     load_and_authorize_resource :class=>'ClearCMS::Content', :except=>[:email]
     
-    
+
+   
     def index
       
       types=[]
@@ -49,7 +49,6 @@ module ClearCMS
       @clear_cms_content.source='web'
       #@clear_cms_content.linked_contents.build
       @clear_cms_content.content_blocks.build(:type=>'raw')
-
   
       respond_to do |format|
         format.html # new.html.erb
@@ -66,7 +65,9 @@ module ClearCMS
     
     def update 
       @clear_cms_content=Content.find(params[:id])
-      # @clear_cms_content.assign_attributes(params[:content])
+
+      #@clear_cms_content.assign_attributes(params[:content])
+      
       @clear_cms_content.content_logs.build(:user=>current_user, :entry=>"edited")
 
       #@clear_cms_content=@clear_cms_content.becomes(params[:content]['_type'].constantize)
@@ -80,20 +81,21 @@ module ClearCMS
       # using with_protection and move on.  Generally speaking, this means we can't use any special subclass of Content functions or validations until it's fixed (and that the validations WILL run for the previous type)
       # since it's instantiated as that by Mongoid BEWARE****
       
+      #if @clear_cms_content.save
       if @clear_cms_content.update_attributes(params[:content].permit!)
         redirect_to({:action=>:edit}, notice: 'Content was successfully updated.')
       else
         flash.now[:notice]='Error saving content!'
         render :action=>:edit
-      end
-      # end         
+      end           
     end
 
 
     def create
       @clear_cms_content = params[:content]['_type'].constantize.new(params[:content].permit!)
+      #@clear_cms_content = Content.new(params[:content])
       @clear_cms_content.content_logs.build(:user=>current_user, :entry=>"created")
-      
+  
       respond_to do |format|
         if @clear_cms_content.save
           format.html { 
@@ -101,13 +103,13 @@ module ClearCMS
           }
           format.json { render json: @clear_cms_content, status: :created, location: clear_cms.content_path(@clear_cms_content)}
         else
-          format.html { 
-            flash.now[:notice]='Error creating content!'
-            render action: "new" 
-          }
+          format.html {
+              flash.now[:notice]='Error creating content!' 
+              render action: "new" 
+            }
           format.json { render json: @clear_cms_content.errors, status: :unprocessable_entity }
         end
-      end  
+      end
     end
     
     
@@ -145,6 +147,7 @@ module ClearCMS
 
     end
 
+    
     def email
   #       message = Mail.new(params[:message])
   #       Rails.logger.log message.subject #print the subject to the logs
@@ -161,6 +164,7 @@ module ClearCMS
   
       render :text => 'success', :status => 200 # a status of 404 would reject the mail      
     end
+  
   end
 end
 
