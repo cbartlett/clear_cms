93b9909a26a29ab95de5fc20414e66f85b00ded6
Stephen Marsh <stephenforpresident@gmail.com>
2014-12-17 00:32:26 -0500

    history view improvements

--- app/models/clear_cms/history_tracker.rb
+++ app/models/clear_cms/history_tracker.rb
@@ -2,13 +2,13 @@ module ClearCMS
   class HistoryTracker
     include Mongoid::History::Tracker
 
-    belongs_to :modifier, class_name: 'ClearCMS::User'
+    # belongs_to :modifier, class_name: 'ClearCMS::User'
 
     def undo_or_redo(change, user)
       if change.downcase == 'undo'
-        self.undo! #user
+        self.undo! user
       elsif change.downcase == 'redo'
-        self.redo! #user
+        self.redo! user
       else
         flash.now[:notice]='Error Undoing or Redoing'
       end
