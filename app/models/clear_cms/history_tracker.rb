d38d3b3651114eab125968ffdb80d33e387a6bdb
Stephen Marsh <stephenforpresident@gmail.com>
2015-01-07 12:13:09 -0500

    got userstamps tracking history objects for all nested documents. sweet.

--- app/models/clear_cms/history_tracker.rb
+++ app/models/clear_cms/history_tracker.rb
@@ -1,6 +1,9 @@
 module ClearCMS
   class HistoryTracker
     include Mongoid::History::Tracker
+    include Mongoid::Userstamp
+
+    mongoid_userstamp user_model: 'ClearCMS::User', created_name: :modifier, updated_name: :modifier
 
     def undo_or_redo(change, user)
       if change.downcase == 'undo'
