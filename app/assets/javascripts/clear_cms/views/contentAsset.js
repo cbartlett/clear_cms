69c46e3bb786b85071c2e17cd272562a852b5db6
Stephen Marsh <stephenforpresident@gmail.com>
2015-02-25 10:59:25 -0500

    renders content_assets as a partial now, that's a start

---
+++ app/assets/javascripts/clear_cms/views/contentAsset.js
@@ -0,0 +1,6 @@
+ClearCMS.ContentAssetView = Ember.View.extend({
+  // We are setting templateName manually here to the default value
+  templateName: "contentAsset",
+})
+
+console.log("loaded bro");
