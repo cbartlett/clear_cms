6014f020b7a05898cd4af261de3718fade930fdf
Stephen Marsh <stephenforpresident@gmail.com>
2015-02-25 11:44:36 -0500

    switching

---
+++ app/assets/javascripts/clear_cms/views/contentBlock.js
@@ -0,0 +1,6 @@
+ClearCMS.ContentBlocktView = Ember.View.extend({
+  // We are setting templateName manually here to the default value
+  // templateName: "contentBlock",
+})
+
+// console.log("loaded bro");
