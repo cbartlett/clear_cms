45af30a1c124b0c7b4d85321c121545051e8be8d
Stephen Marsh <stephenforpresident@gmail.com>
2015-03-04 13:03:32 -0500

    removing unneeded belongsTo that put parent ids in nested documents

--- app/assets/javascripts/clear_cms/models/contentAsset.js
+++ app/assets/javascripts/clear_cms/models/contentAsset.js
@@ -1,6 +1,5 @@
 ClearCms.ContentAsset = DS.Model.extend({
   primaryKey: '_id',
-  content_block: DS.belongsTo('content_block', {embedded: 'always'}),
   file: DS.attr('string'),
   path: DS.attr('string'),
   mounted_file: DS.attr(),
