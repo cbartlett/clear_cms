ef4da2a9403e0f90ecdfb380ec984595181f2ce6
Joel Niedfeldt <joel@niedfeldt.com>
2015-03-10 18:52:38 -0400

    trying to change json output

--- app/assets/javascripts/clear_cms/serializers/content.js
+++ app/assets/javascripts/clear_cms/serializers/content.js
@@ -2,5 +2,18 @@ ClearCms.ContentSerializer = ClearCms.ApplicationSerializer.extend({
   attrs: {
     content_blocks: { embedded: 'always' },
   },
+  serialize: function(snapshot, options) {
+	var json = this._super(snapshot, options);
+
+	json.content_blocks_attributes = json.content_blocks; 
+	delete json.content_blocks; 
+
+	forEach(json.content_blocks_attributes, function(item) {
+		item.content_assets_attributes = item.content_assets;
+		delete item.content_assets; 
+	});
+
+	return json;
+	},
 });
 
