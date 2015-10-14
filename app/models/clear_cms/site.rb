7c706bc472306000fac12b94fe770f70292461d1
Stephen Marsh <stephenforpresident@gmail.com>
2015-10-14 15:44:15 -0400

    refactor categories method to use aggregation pipeline instead of map-reduce

--- app/models/clear_cms/site.rb
+++ app/models/clear_cms/site.rb
@@ -4,63 +4,44 @@ module ClearCMS
     #include Mongoid::Versioning
     # keep at most 5 versions of a record
     #max_versions 5
-    
+
     after_destroy :remove_permissions
-    
-    
+
+
     has_many :contents, class_name: 'ClearCMS::Content', :inverse_of => :site #, :dependent => :destroy
-    
+
     field :name
     field :domain
-    field :slug 
+    field :slug
     field :keywords
     field :description
     field :language
-    
+
     field :source_id
     field :source_attributes, type: Hash
-    
+
     validates_presence_of :name
-    
+
     validates_presence_of :slug
     validates_uniqueness_of :slug
-    
+
     validates_presence_of :domain
     validates_uniqueness_of :domain
-    
-    
+
+
     def self.content_columns
       fields.collect {|f| f[1]}
     end
-    
-    
-    def categories
-      map = %Q{
-        function() {
-           exclude_categories=/(view_by|[0-9])/i;
-           for (var idx = 0; idx < this.categories.length; idx++) {
-               var key = this.categories[idx];
-               if(!(exclude_categories.test(key))) {
-                emit(key, {count: 1});
-               }
-           }
-        }
-      }
-      
-      reduce = %Q{
-        function(key, values) {
-          var result = { count: 0 };
-          values.forEach(function(value) {
-            result.count += value.count;
-          });
-          return result;
-        }
-      }
 
-      contents.count > 1 ? contents.map_reduce(map,reduce).out(inline:true) : []
+    def categories
+      ClearCMS::Content.collection.aggregate([
+        {"$match" => {"site_id" => self.id}},
+        {"$unwind" => "$categories"},
+        {"$group" => {_id: "$categories", :count => {"$sum" => 1}}},
+        {"$sort" => {_id: 1}}])
     end
 
-private 
+private
 
     def remove_permissions
       ClearCMS::User.where('permissions.site_id'=>self.id).each do |u|
@@ -68,6 +49,6 @@ private
       end
     end
 
-      
+
   end
 end
